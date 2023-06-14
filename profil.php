d562ab8481841bf27f47ff23f95aca300262a261
cedric-prunier <cedric-prunier@laplateforme.io>
2023-06-14 16:41:46 +0200

    premier

--- profil.php
+++ profil.php
@@ -45,28 +45,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $sql = "UPDATE utilisateurs SET login = '$nouveaulogin', password = '$nouveaupassword' WHERE login = '$login'";
 
     if ($conn->query($sql) === TRUE) {
-        echo "Informations mises à jour avec succès.";
+        $message = "Informations mises à jour avec succès.";
         // Mettre à jour les valeurs dans la session
         $_SESSION["login"] = $nouveaulogin;
         $_SESSION["password"] = $nouveaupassword;
-
-
     } else {
-        echo "Erreur lors de la mise à jour des informations : " . $conn->error;
+        $message = "Erreur lors de la mise à jour des informations : " . $conn->error;
     }
-
 }
 
 // Fermeture de la connexion à la base de données
 $conn->close();
 ?>
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <title>Modifier le profil</title>
-    <link rel="stylesheet" href="profil.css">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Accueil</title>
+    <link rel="stylesheet" href="./profil.css" />
+    <link rel="shortcut icon" href="./images/shortcut_icon.png" />
 </head>
 
 <body>
@@ -74,7 +74,6 @@ $conn->close();
 
         <ul class="navlinks">
             <?php
-
             if (isset($_SESSION['login'])) {
                 // Utilisateur connecté
                 echo 'Bonjour, ' . $_SESSION['login'];
@@ -83,9 +82,9 @@ $conn->close();
                 echo '<li class="projet"><a href="commentaire.php">Commentaire</a></li>';
                 echo '<li class="projet"><a href="profil.php">Profil</a></li>';
                 echo '<li><a href="logout.php">Déconnexion</a></li>';
-
             } else {
                 // Aucun utilisateur connecté
+                echo '<li class="projet"><a href="livre-or.php">Livre d\'Or</a></li>';
                 echo '<li class="projet"><a href="inscription.php">Inscription</a></li>';
                 echo '<li class="projet"><a href="connexion.php">Connexion</a></li>';
             }
@@ -95,57 +94,55 @@ $conn->close();
             <span></span>
         </div>
     </nav>
-
     <form class="formulaire" action="profil.php" method="post">
-
         <h1>Modifier votre profil</h1>
-        <br>
+        <?php if (isset($message)) { ?>
+            <p>
+                <?php echo $message; ?>
+            </p>
+        <?php } ?>
         <label for="login">Login</label>
-
         <input type="text" id="login" name="login" value="<?php echo $login; ?>" required>
         <label for="password">Mot de passe</label>
-
         <input type="password" id="password" name="password" value="<?php echo $password; ?>" required>
         <button class="eye" type="button" onclick="togglePassword()" id="toggle-password"><img
                 src="./images/eye-open.svg" alt="" /></button>
-
-
         <input type="submit" name="valider" value="Valider &#10004;" />
-
     </form>
 
+    <script>
+        const burger = document.querySelector(".burger");
+        const navlinks = document.querySelector(".navlinks");
+        const body = document.querySelector("body");
+
+        burger.addEventListener("click", () => {
+            navlinks.classList.toggle("mobile-menu");
+            burger.classList.toggle("cross");
+        });
+
+        body.addEventListener("click", (event) => {
+            if (!burger.contains(event.target) && !navlinks.contains(event.target)) {
+                navlinks.classList.remove("mobile-menu");
+                burger.classList.remove("cross");
+            }
+        });
+    </script>
+
+    <script>
+        function togglePassword() {
+            const passwordInput = document.getElementById("password");
+            const togglePasswordButton = document.getElementById("toggle-password");
+            const img = togglePasswordButton.querySelector("img");
+
+            if (passwordInput.type === "password") {
+                passwordInput.type = "text";
+                img.src = "./images/eye-closed.svg";
+            } else {
+                passwordInput.type = "password";
+                img.src = "./images/eye-open.svg";
+            }
+        }
+    </script>
 </body>
-<script>
-    const burger = document.querySelector(".burger");
-    const navlinks = document.querySelector(".navlinks");
-    const body = document.querySelector("body");
-
-    burger.addEventListener("click", () => {
-        navlinks.classList.toggle("mobile-menu");
-        burger.classList.toggle("cross");
-    });
-
-    body.addEventListener("click", (event) => {
-        if (!burger.contains(event.target) && !navlinks.contains(event.target)) {
-            navlinks.classList.remove("mobile-menu");
-            burger.classList.remove("cross");
-        }
-    });
-</script>
-<script>
-    function togglePassword() {
-        const passwordInput = document.getElementById("password");
-        const togglePasswordButton = document.getElementById("toggle-password");
-        const img = togglePasswordButton.querySelector("img");
-
-        if (passwordInput.type === "password") {
-            passwordInput.type = "text";
-            img.src = "./images/eye-closed.svg";
-        } else {
-            passwordInput.type = "password";
-            img.src = "./images/eye-open.svg";
-        }
-    }
-</script>
 
 </html>
